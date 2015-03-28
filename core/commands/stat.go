8d4de6d42b17261fe3270ebc2a3e5988dcd40e09
Jeromy <jeromyj@gmail.com>
2015-03-28 14:56:25 -0700

    add poll command for continuous bandwidth reporting

--- core/commands/stat.go
+++ core/commands/stat.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"time"
 
 	humanize "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/dustin/go-humanize"
 
@@ -12,6 +13,7 @@ import (
 	metrics "github.com/jbenet/go-ipfs/metrics"
 	peer "github.com/jbenet/go-ipfs/p2p/peer"
 	protocol "github.com/jbenet/go-ipfs/p2p/protocol"
+	u "github.com/jbenet/go-ipfs/util"
 )
 
 var StatsCmd = &cmds.Command{
@@ -21,7 +23,8 @@ var StatsCmd = &cmds.Command{
 	},
 
 	Subcommands: map[string]*cmds.Command{
-		"bw": statBwCmd,
+		"bw":      statBwCmd,
+		"bw-poll": statBwPollCmd,
 	},
 }
 
@@ -93,6 +96,77 @@ var statBwCmd = &cmds.Command{
 			fmt.Fprintf(out, "RateIn: %s/s\n", humanize.Bytes(uint64(bs.RateIn)))
 			fmt.Fprintf(out, "RateOut: %s/s\n", humanize.Bytes(uint64(bs.RateOut)))
 			return out, nil
+		},
+	},
+}
+
+var statBwPollCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline:          "Print ipfs bandwidth information continuously",
+		ShortDescription: ``,
+	},
+	Run: func(req cmds.Request, res cmds.Response) {
+		nd, err := req.Context().GetNode()
+		if err != nil {
+			res.SetError(err, cmds.ErrNormal)
+			return
+		}
+
+		// Must be online!
+		if !nd.OnlineMode() {
+			res.SetError(errNotOnline, cmds.ErrClient)
+			return
+		}
+
+		out := make(chan interface{})
+		go func() {
+			defer close(out)
+			tick := time.NewTicker(time.Second)
+			for {
+				select {
+				case <-req.Context().Context.Done():
+					return
+				case <-tick.C:
+					totals := nd.Reporter.GetBandwidthTotals()
+					out <- &totals
+				}
+			}
+		}()
+
+		res.SetOutput((<-chan interface{})(out))
+	},
+	Type: metrics.Stats{},
+	Marshalers: cmds.MarshalerMap{
+		cmds.Text: func(res cmds.Response) (io.Reader, error) {
+			outCh, ok := res.Output().(<-chan interface{})
+			if !ok {
+				return nil, u.ErrCast()
+			}
+
+			first := true
+			marshal := func(v interface{}) (io.Reader, error) {
+				bs, ok := v.(*metrics.Stats)
+				if !ok {
+					return nil, u.ErrCast()
+				}
+				out := new(bytes.Buffer)
+				if first {
+					fmt.Fprintln(out, "Total Up\t Total Down\t Rate Up\t Rate Down")
+					first = false
+				}
+				fmt.Fprint(out, "\r                                                \r")
+				fmt.Fprintf(out, "%s\t\t", humanize.Bytes(uint64(bs.TotalOut)))
+				fmt.Fprintf(out, " %s\t\t", humanize.Bytes(uint64(bs.TotalIn)))
+				fmt.Fprintf(out, " %s/s \t", humanize.Bytes(uint64(bs.RateOut)))
+				fmt.Fprintf(out, " %s/s", humanize.Bytes(uint64(bs.RateIn)))
+				return out, nil
+
+			}
+
+			return &cmds.ChannelMarshaler{
+				Channel:   outCh,
+				Marshaler: marshal,
+			}, nil
 		},
 	},
 }
